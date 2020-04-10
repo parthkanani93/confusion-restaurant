609c01b0924e717c61821c21e46239a6162bcbd7
parth <parthkanani8490@gmail.com>
2020-04-10 01:35:27 -0700

    combining reducer in redux

--- src/Redux/configerStore.js
+++ src/Redux/configerStore.js
@@ -1,9 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialstate } from './Reducer';
+import { createStore,combineReducers } from 'redux';
+import {Comments} from './comment';
+import {Dishes} from './dishes';
+import {Leaders} from './leaders';
+import {Promotions} from './promotions';
 
 export const  configerStore = () => {
 
-    const store = createStore(Reducer, initialstate);
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+     );
 
     return store;
 }
---
+++ src/Redux/promotions.js
@@ -0,0 +1,12 @@
+import { PROMOTIONS } from '../shared/promotion';
+
+export const Promotions=(state=PROMOTIONS,action)=>{
+    switch (action.type) {
+      
+        default:
+            return state;
+            
+    }
+
+};
+
